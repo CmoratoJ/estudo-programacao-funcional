d2f20dd560f49ad111b676b4f2ef59ad0a189550
Caio Morato <caiomorato94@hotmail.com>
2024-07-19 12:24:58 -0300

    first commit

---
+++ brincadeira.php
@@ -0,0 +1,15 @@
+<?php
+$variavel = 'teste';
+
+function outra(callable $funcao): void
+{
+    echo 'Executando outra função: ';
+    echo $funcao();
+}
+
+$nomeDaFuncao = function () use ($variavel) {
+    echo $variavel;
+    return 'Uma outra função:';
+};
+
+outra($nomeDaFuncao);
---
+++ manipulacao.php
@@ -0,0 +1,49 @@
+<?php
+
+$dados = require 'dados.php';
+
+$contador = count($dados);
+
+echo "Número de países: $contador\n";
+
+function convertePaisParaLetraMaiuscula (array $pais): array
+{
+    $pais['pais'] = mb_convert_case($pais['pais'], MB_CASE_UPPER);
+    return $pais;
+}
+
+$somaMedalhas = fn (int $medalhasAcumuladas, int $medalhas): int => $medalhasAcumuladas + $medalhas;
+
+$medalhasAcumuladas = fn (int $medalhasAcumuladas, array $pais): int 
+    => $medalhasAcumuladas + array_reduce($pais['medalhas'], $somaMedalhas, 0);
+
+$verificaSePaisTemEspacoNoNome = fn (array $pais): bool => str_contains($pais['pais'], ' ');
+
+$comparaMedalhas = fn (array $medalhasPais1, array $medalhasPais2) 
+    => fn (string $modalidade): int => $medalhasPais2[$modalidade] <=> $medalhasPais1[$modalidade];
+
+$dados = array_map('convertePaisParaLetraMaiuscula', $dados);
+$dados = array_filter($dados, $verificaSePaisTemEspacoNoNome);
+
+$medalhas = array_reduce(
+    array_map(
+        fn (array $medalhas): int => array_reduce($medalhas, $somaMedalhas, 0), 
+        array_column($dados, 'medalhas')
+    ), $somaMedalhas, 0
+);
+
+usort($dados, function (array $pais1, array $pais2) use ($comparaMedalhas) {
+    $medalhasPais1 = $pais1['medalhas'];
+    $medalhasPais2 = $pais2['medalhas'];
+
+    $comparador = $comparaMedalhas($medalhasPais1, $medalhasPais2);
+
+    return $comparador('ouro') !== 0 ? $comparador('ouro') 
+        : ($comparador('prata') !== 0 ? $comparador('prata') 
+        : $comparador('bronze')
+    );
+});
+
+var_dump($dados);
+
+echo $medalhas;
